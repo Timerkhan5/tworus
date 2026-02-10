ee6d64b66d8cdc619b2805dfbe91eddaa8597630
Timerkhan5 <tkurtashkin05@gmail.com>
2026-02-10 13:21:40 +0300

    all_domens

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors({
     origin: [
-        'https://tworus.pages.dev',
+        '*',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
